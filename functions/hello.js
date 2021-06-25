486c1dca173892f9283d8483ec34ffe98772ce03
samrat azad <samrat0131@gmail.com>
2021-06-26 03:42:48 +0600

    update

--- functions/hello.js
+++ functions/hello.js
@@ -2,6 +2,12 @@ exports.handler = async event => {
   const subject = event.queryStringParameters.name || 'World'
   return {
     statusCode: 200,
-    body: `Hello ${subject}!`,
+    body: JSON.stringify(
+      {
+        id: 1,
+        title: "hello",
+        content: "lorem ipsum dolor sit amet"
+      }
+    ),
   }
 }
