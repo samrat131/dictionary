4be3ecf75697bcbb64b0017729206faef44ff987
samrat azad <samrat0131@gmail.com>
2021-06-26 03:45:06 +0600

    updaet

--- functions/hello.js
+++ functions/hello.js
@@ -3,11 +3,21 @@ exports.handler = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
-      {
+      [{
         id: 1,
         title: "hello",
         content: "lorem ipsum dolor sit amet"
-      }
+      },
+      {
+        id: 2,
+        title: "hello 2",
+        content: "lorem ipsum dolor sit amet 2"
+      },
+      {
+        id: 3,
+        title: "hello 3",
+        content: "lorem ipsum dolor sit amet 3"
+      }]
     ),
   }
 }
