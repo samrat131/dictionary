d8dd764ee699ecabaa9654d2813a3ea192e0e9a5
samrat azad <samrat0131@gmail.com>
2021-07-01 03:38:43 +0600

    checking mongodb conn

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,18 @@
+const mongoose = require('mongoose');
+
 exports.handler = async event => {
+  
   const subject = event.queryStringParameters.name || 'World'
+
+  mongoose.connect('mongodb+srv://sam123:sam123@cluster0.6io27.mongodb.net/nodejs_api_db?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }, () => {
+    console.log('db connected');
+  });
+
   return {
     statusCode: 200,
     body: JSON.stringify(
