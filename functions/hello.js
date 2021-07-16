13eda102d62243675131f5495d8d3e3c86261578
samrat azad <samrat0131@gmail.com>
2021-07-17 03:38:01 +0600

    update

--- functions/hello.js
+++ functions/hello.js
@@ -1,31 +1,12 @@
-const { MongoClient } = require("mongodb");
-
-const uri =
-  "mongodb+srv://sam123:sam123@cluster0.6io27.mongodb.net?retryWrites=true&w=majority";
-
-const client = new MongoClient(uri);
-
 exports.handler = async function (event, context) {
-  try {
-    await client.connect();
 
-    const database = client.db('nodejs_api_db');
-    const posts = database.collection('Posts');
+  const post = [{
+    title: "hello world",
+    body: "this the content"
+  }];
 
-    const query = {};
-    const post = await posts.findOne(query);
-    await client.close();
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(post)
-    }
-
-  } catch (error) {
-    
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error)
-    }
-  } 
+  return {
+    statusCode: 200,
+    body: JSON.stringify(post)
+  }
 }
