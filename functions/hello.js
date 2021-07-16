2f7b46b0fdc1e8220243eb56ab7ac11302efea6f
samrat azad <samrat0131@gmail.com>
2021-07-17 03:44:14 +0600

    update

--- functions/hello.js
+++ functions/hello.js
@@ -1,3 +1,30 @@
+const { MongoClient } = require('mongodb')
+
+// Connection URL
+const url = 'mongodb+srv://sam123:sam123@cluster0.6io27.mongodb.net/nodejs_api_db?retryWrites=true&w=majority'
+const client = new MongoClient(url)
+
+// Database Name
+const dbName = 'nodejs_api_db'
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect()
+  console.log('Connected successfully to server')
+  const db = client.db(dbName)
+  const collection = db.collection('posts')
+
+  const findResult = await collection.find({}).toArray()
+  console.log('Found documents =>', findResult)
+
+  return 'done.'
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close())
+
 exports.handler = async function (event, context) {
 
   const post = [{
@@ -7,6 +34,6 @@ exports.handler = async function (event, context) {
 
   return {
     statusCode: 200,
-    body: JSON.stringify(post)
+    body: JSON.stringify(findResult)
   }
 }
