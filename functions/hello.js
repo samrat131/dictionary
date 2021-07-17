3d5bb51ca8694cd94f1036cb2b7cd118e6be3c31
samrat azad <samrat0131@gmail.com>
2021-07-18 02:54:12 +0600

    added dotenv

--- functions/hello.js
+++ functions/hello.js
@@ -1,21 +1,26 @@
 const { MongoClient } = require('mongodb')
 
-// Connection URL
-const url = 'mongodb+srv://sam123:sam123@cluster0.6io27.mongodb.net/nodejs_api_db?retryWrites=true&w=majority'
-const client = new MongoClient(url)
+const client = new MongoClient(process.env.DB_CONNECTION_URL)
 
 exports.handler = async function (event, context) {
 
   await client.connect()
   const db = client.db('nodejs_api_db')
   const collection = db.collection('posts')
+
+  const insertResult = await collection.insertMany([
+    {
+      title: "hello this is test",
+      body: "this is the body"
+    },
+    {
+      title: "hello this is test2",
+      body: "this is the body2"
+    }
+  ])
+
   const findResult = await collection.find({}).toArray()
   client.close()
-
-  // const post = [{
-  //   title: "hello world",
-  //   body: "this the content"
-  // }];
 
   return {
     statusCode: 200,
