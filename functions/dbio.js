817605bd6551c97b50fc7b19d101a469db3b8dee
samrat azad <samrat0131@gmail.com>
2021-07-19 03:53:07 +0600

    rename file

---
+++ functions/dbio.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require('mongodb')
+require('dotenv').config();
+
+const client = new MongoClient(process.env.DB_CONNECTION_URL)
+
+exports.handler = async function (event, context) {
+
+  const mode = event.queryStringParameters.mode;
+  const eng = event.queryStringParameters.eng || null;
+  const ban = event.queryStringParameters.ban || null;
+
+  await client.connect()
+  const db = client.db('dictionary')
+  const collection = db.collection('words')
+
+  if (mode == 'write') {
+    await collection.insertOne([
+      {
+        english: eng,
+        bangla: ban
+      }
+    ])  
+  }
+
+  let findResult
+  if (mode == 'read') {
+    findResult = await collection.find({}).toArray()
+  }
+
+  client.close()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(findResult)
+  }
+}
