d6243de3f863729d889a52d572da89457d1d0c08
samrat azad <samrat0131@gmail.com>
2021-07-19 03:51:37 +0600

    read write op added

--- functions/hello.js
+++ functions/hello.js
@@ -5,22 +5,28 @@ const client = new MongoClient(process.env.DB_CONNECTION_URL)
 
 exports.handler = async function (event, context) {
 
+  const mode = event.queryStringParameters.mode;
+  const eng = event.queryStringParameters.eng || null;
+  const ban = event.queryStringParameters.ban || null;
+
   await client.connect()
   const db = client.db('dictionary')
   const collection = db.collection('words')
 
-  const insertResult = await collection.insertMany([
-    {
-      english: "Incarcerate",
-      bangla: "কারারুদ্ধ করা"
-    },
-    {
-      english: "Upbringing",
-      bangla: "lalon palon kora"
-    }
-  ])
+  if (mode == 'write') {
+    await collection.insertOne([
+      {
+        english: eng,
+        bangla: ban
+      }
+    ])  
+  }
 
-  const findResult = await collection.find({}).toArray()
+  let findResult
+  if (mode == 'read') {
+    findResult = await collection.find({}).toArray()
+  }
+
   client.close()
 
   return {
